d7173e41ed89728e59e9236e4652413a663b77a4
Sneha Kumari <sneha.kumari@northwestern.edu>
2019-06-18 19:06:23 -0500

    trying

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ChicagoRestaurantInspections
+
+Steps to run this repo.
+
+1. Generate database using the Run app.py
+2. 
--- static/js/logic.js
+++ static/js/logic.js
@@ -13,7 +13,15 @@ var myMap = L.map("map", {
 
   // Store API query variables
   var chartURL = "/data";
- 
+
+  // get walk score for given restaurant
+  function getWalkScore(lat, lon, address) {
+    var address=encodeURI(address);
+    console.log(address)
+    var baseUrl = `http://api.walkscore.com/score?format=json&address${address}&lat=${lat}&lon=${lon}&wsapikey=432a8921bb7e0a9c81ba0a01a48b5807&limit=10`;
+  //   $str = @file_get_contents($url); 
+    return baseUrl; 
+  } 
   // Grab the data with d3
   d3.json(chartURL, function(response) {
     //console.log(response);
@@ -29,15 +37,29 @@ var myMap = L.map("map", {
 
     // Check for location property
     if (location) {
+      lat = response[i].Lat
+      lon = response[i].Lng
+      address = str(response[i].Street + " "+ response[i].City +  " "+ response[i].State +  " "+ response[i].Zip)
+      address = stripslashes($address);
+      $.getJSON = getWalkScore(lat,lon,address);
+      console.log(json);
 
       // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([response[i].Lng, response[i].Lat])
-      .bindPopup(response[i].DBA  + response[i].success + response[i].Inspection + response[i].Year))
+      markers.addLayer(L.marker([lon, lat])
+      .bindPopup("Name: "+response[i].DBA  +"<br>Status: "+ response[i].success + " <br>Inspection: "+response[i].Inspection +"<br>Year: "+ response[i].Year))
     
     }
-    console.log(response[i].Lat, response[1].Lng)
+    // console.log(response[i].Lat, response[i].Lng)
   }
   // Add our marker cluster layer to the map
   myMap.addLayer(markers);
 
 });
+
+// // Leaflet -- show latitude logitude for any point clicked on map
+// marker.on('click', function(ev){
+//   var latlng = map.mouseEventToLatLng(ev.originalEvent);
+//   console.log(latlng.lat + ', ' + latlng.lng);
+// });
+
+
